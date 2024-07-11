7ae7533697388414198b34ff76c425ba0b93a724
Ayman Jundi <ajundi@gmail.com>
2024-07-11 01:01:27 -0400

    Added home-manager config file using default nix run home-manager -- init
    Signed-off-by: Ayman Jundi <ajundi@gmail.com>

--- flake.nix
+++ flake.nix
@@ -10,13 +10,25 @@
     };
   };
 
-  outputs = { nixpkgs, ... } @ inputs:
-  {
+  outputs = { nixpkgs,home-manager, ... } @ inputs:
+   let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix
       ];
     };
+      homeConfigurations."hoid" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
 
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+    };
   };
 }
