37c7065b30d8ea24addc0803a00ae953a69f64af
Ayman Jundi <ajundi@gmail.com>
2024-07-10 22:52:58 -0400

    Flake.nix is now the main entry to rebuild the system using sudo nixos-rebuild switch --flake .
    Signed-off-by: Ayman Jundi <ajundi@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  description = "A very basic flake";
+  description = "Main Entry Point";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  outputs = { nixpkgs, ... } @ inputs:
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
 
   };
 }
