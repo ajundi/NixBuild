35b97d0b695146ddf924fae174506d2cb0655315
Ayman Jundi <ajundi@gmail.com>
2024-08-07 20:37:05 -0400

    added encironmental variable for steam library folder.
    9 current   2024-08-07 20:36:43  24.11.20240804.cb9a96f   6.6.44                          *

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,7 @@
     EDITOR = "codium --wait";
     SUDO_EDITOR = "kate";
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/../shared/.steam/root/compatibilitytools.d";
+    STEAM_LIBRARY_FOLDERS = "\${HOME}/../shared/.steam";
   };
 
   # Enable automatic login for the user.
